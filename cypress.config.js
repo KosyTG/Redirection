420fa851f09935f683a1894970ba31ffe993fea8
emmanuel <emmanuelkosi19@gmail.com>
2024-09-08 14:30:27 +0100

    initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://the-internet.herokuapp.com/",
+    baseUrl: "https://the-internet.herokuapp.com",
     viewportWidth: 900,
     viewportHeight: 1680,
+    defaultCommandTimeout: 30000,
+    watchForFileChanges: false,
     chromeWebSecurity: false,
+    // experimentalModifyObstructiveThirdPartyCode: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/redirection/redirection.spec.cy.js
+++ cypress/e2e/redirection/redirection.spec.cy.js
@@ -1,3 +1,12 @@
 describe('Testing redirection', ()=>{
+    it('Using cross origin command', ()=>{
+
+        cy.visit('/')
+        cy.get("a[href='http://elementalselenium.com/']").invoke('removeAttr','target').should('exist').click()
+        cy.origin('https://elementalselenium.com/', ()=>{
+                    cy.get("#email").should('exist').type('kositg22@gmail.com')
+                })
+
+    })
     
 })
