fc0028d29d6b0d6086e6d1c257cc795d4e2e6c51
emmanuel <emmanuelkosi19@gmail.com>
2024-09-09 07:57:09 +0100

    initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: "https://the-internet.herokuapp.com",
     viewportWidth: 900,
     viewportHeight: 1680,
-    defaultCommandTimeout: 30000,
+    defaultCommandTimeout: 20000,
     watchForFileChanges: false,
     chromeWebSecurity: false,
     // experimentalModifyObstructiveThirdPartyCode: true,
--- cypress/e2e/redirection/redirection.spec.cy.js
+++ cypress/e2e/redirection/redirection.spec.cy.js
@@ -1,12 +1,14 @@
 describe('Testing redirection', ()=>{
     it('Using cross origin command', ()=>{
+        cy.fixture('selectors').then((data)=>{
+            cy.get(data.elementSelenium).invoke('removeAttr','target').should('exist').click()
+            cy.origin('https://elementalselenium.com/', { args: {data}}, ({data})=>{
+                cy.get(data.emailTextfield).should('exist').type('kositg22@gmail.com')
+                cy.get(data.pickLanguage).select('javascript')
+            })
+            
+        })
 
-        cy.visit('/')
-        cy.get("a[href='http://elementalselenium.com/']").invoke('removeAttr','target').should('exist').click()
-        cy.origin('https://elementalselenium.com/', ()=>{
-                    cy.get("#email").should('exist').type('kositg22@gmail.com')
-                })
+  })
 
-    })
-    
 })
